b6d54ba7ff0dbefda6d1e992b1889ac6965e5ea7
Thomas <thomas.giovannoni@hotmail.fr>
2019-11-17 13:41:39 +0100

    added sounds r2, intro + anim

---
+++ Project Java/Assets/Scripts/TitleUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleUI : MonoBehaviour
+{
+    public Animator missionDescription;
+
+    public void DiplsayMissionDescription()
+    {
+        missionDescription.gameObject.SetActive(true);
+    }
+
+    public void InitLevel()
+    {
+        missionDescription.gameObject.SetActive(false);
+    }
+}
