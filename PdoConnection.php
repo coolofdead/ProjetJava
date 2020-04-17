a82c8dd32546eb53b69cbebddc4073a2661ba671
Thomas GIOVANNONI <45070722+coolofdead@users.noreply.github.com>
2020-04-17 19:01:13 +0200

    Update PdoConnection.php

--- PdoConnection.php
+++ PdoConnection.php
@@ -1,11 +1,13 @@
 <?php
 
 $host = 'localhost';
-$dbName = 'starwars;';
+$port = '3308';
+$dbName = 'starwars';
 $user = 'root';
+$password = '';
 
 try {
-    $bdd = new PDO('mysql:host=' . $host . ';dbname=' . $dbName . 'charset=utf8', $user);
+    $bdd = new PDO("mysql:dbname=$dbName;host=$host;port=$port", $user, $password);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
